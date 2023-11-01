d4d14bf47222b5c319fa57538d4b25412dd74c8e
Antonio <149367770+Slaidness@users.noreply.github.com>
2023-11-01 18:09:47 +0100

    Structs and view for User and RunData
    HydroRun0_01App is the main, User and RunData are structs, Content view show the data from User and RunData.
    
    RunData still need a revision and make the info see in the ContentView

---
+++ HydroRun0_01App.swift
@@ -0,0 +1,17 @@
+//
+//  HydroRun0_01App.swift
+//  HydroRun0.01
+//
+//  Created by Antonio on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HydroRun0_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
