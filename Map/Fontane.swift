96c6f4582d194bd729c76a238c94a38e2c3d27ea
Antonio <147817129+AntonioTri@users.noreply.github.com>
2023-11-01 12:28:36 +0100

    Fountains
    Fountains with name and coordinates

--- Map/Fontane.swift
+++ Map/Fontane.swift
@@ -6,21 +6,20 @@ import MapKit
 struct Fontane: Identifiable {
     let id = UUID()
     let coordinate: CLLocationCoordinate2D
+    let nome: String
 }
 
 //Dichiariamo qui un array di struct Fontanelle, ad ognuna assegnamo le sue corrispettive coordinate di
 //Latitudine e Longitudine
 let fontane: [Fontane] = [
 
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.865639, longitude: 14.249892)),
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.86977, longitude: 14.250434)),
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.869202, longitude: 14.251986)),
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.876592, longitude: 14.250561)),
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.876547, longitude: 14.25154)),
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.875176, longitude: 14.253907)),
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.877143, longitude: 14.257095)),
-    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.878857, longitude: 14.250253))
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.865639, longitude: 14.249892), nome: "Lecci"),
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.86977, longitude: 14.250434), nome: "Porta Piccola"),
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.869202, longitude: 14.251986), nome: "Porta di Mezzo"),
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.876592, longitude: 14.250561), nome: "Fagianeria"),
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.876547, longitude: 14.25154), nome: "Capraia"),
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.875176, longitude: 14.253907), nome: "Fontana di Mezzo"),
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.877143, longitude: 14.257095), nome: "Fabricato S.Gennaro"),
+    Fontane(coordinate: CLLocationCoordinate2D(latitude: 40.878857, longitude: 14.250253), nome: "Casina della Regina")
 
 ]
-
-
